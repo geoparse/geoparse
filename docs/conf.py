38851acd6d3edec90bd6ff43c4d97807f437304e
Abbas Kiasari <kiasari@gmail.com>
2024-11-04 01:20:39 +0000

    Update docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ release = '0.2.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon", "sphinx_rtd_theme"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon"]
  
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
