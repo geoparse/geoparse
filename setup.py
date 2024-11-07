c8160f4201a1616414bf74be3ff2826080efcd74
Abbas Kiasari <kiasari@gmail.com>
2024-11-07 18:59:39 +0000

    Update packages

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import setuptools
 
 setuptools.setup(
-    name="geoparse",
-    version="0.2.0",
+    name="GeoParse",
+    version="0.3.0",
     python_requires=">=3.10",
     packages=["geoparse"],
 )
