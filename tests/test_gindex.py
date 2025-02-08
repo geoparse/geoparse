d04d9dfb77f4cd645287a33c3b3d7574fb47afd2
Abbas Kiasari <kiasari@gmail.com>
2025-02-08 19:21:54 +0000

    Add test for cellpoint

--- tests/test_gindex.py
+++ tests/test_gindex.py
@@ -1,6 +1,6 @@
 import unittest
 
-from geoparse.gindex import pointcell
+from geoparse.gindex import cellpoint, pointcell
 
 
 class TestPointCell(unittest.TestCase):
@@ -35,6 +35,36 @@ class TestPointCell(unittest.TestCase):
     def test_invalid_cell_type(self):
         with self.assertRaises(ValueError):
             pointcell(self.lats, self.lons, "invalid", 6)
+
+
+class TestCellPoint(unittest.TestCase):
+    def test_geohash(self):
+        result = cellpoint(["ezs42", "u4pruydqqvj"], cell_type="geohash")
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), 2)
+        self.assertTrue(all(isinstance(x, tuple) and len(x) == 2 for x in result))
+
+    def test_h3(self):
+        result = cellpoint(["8928308280fffff"], cell_type="h3")
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), 1)
+        self.assertTrue(isinstance(result[0], tuple) and len(result[0]) == 2)
+
+    def test_s2_int(self):
+        result = cellpoint([9744573459660040192], cell_type="s2_int")
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), 1)
+        self.assertTrue(isinstance(result[0], tuple) and len(result[0]) == 2)
+
+    def test_s2(self):
+        result = cellpoint(["89c25c"], cell_type="s2")
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), 1)
+        self.assertTrue(isinstance(result[0], tuple) and len(result[0]) == 2)
+
+    def test_invalid_cell_type(self):
+        with self.assertRaises(ValueError):
+            cellpoint(["invalid"], cell_type="unknown")
 
 
 if __name__ == "__main__":
