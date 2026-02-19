e0c6d85b738fb7490205955be20b797383fa775c
Abbas Kiasari <kiasari@gmail.com>
2026-02-19 19:16:47 +0000

    Add speeding legend

--- geoparse/geoparse.py
+++ geoparse/geoparse.py
@@ -210,6 +210,93 @@ class Karta:
         return palette[idx]
 
     @staticmethod
+    def _create_speeding_legend():
+        speeding_legend_html = """
+        <!doctype html>
+        <html lang="en">
+        <head>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <title>jQuery UI Draggable - Default functionality</title>
+          <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+          <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+          <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+          <script>
+          $( function() {
+            $( "#maplegend" ).draggable({
+                            start: function (event, ui) {
+                                $(this).css({
+                                    right: "auto",
+                                    top: "auto",
+                                    bottom: "auto"
+                                });
+                            }
+                        });
+        });
+          </script>
+        </head>
+        <body>
+        <div id='maplegend' class='maplegend'
+            style='position: absolute; z-index:9999; border:2px solid grey; background-color:rgba(255, 255, 255, 1);
+             border-radius:6px; padding: 10px; font-size:14px; right: 95px; top: 10px;'>
+        <div class='legend-scale'>
+          <ul class='legend-labels'>
+            <li><span style='background:black;'></span>Speeding ≥ 40% </li>
+            <li><span style='background:red;'></span>30% ≤ Speeding < 40%</li>
+            <li><span style='background:orange;'></span>20% ≤ Speeding < 30%</li>
+            <li><span style='background:yellow;'></span>10% ≤ Speeding < 20%</li>
+            <li><span style='background:green;'></span>0 < Speeding < 10%</li>
+            <li><span style='background:blue;'></span>No speeding</li>
+            <li><span style='background:purple;'></span>Speed limit unavailable</li>
+          </ul>
+        </div>
+        </div>
+        </body>
+        </html>
+        <style type='text/css'>
+          .maplegend .legend-title {
+            text-align: left;
+            margin-bottom: 0;
+            font-weight: bold;
+            font-size: 90%;
+            }
+          .maplegend .legend-scale ul {
+            margin: 0;
+            margin-bottom: 0;
+            padding: 0;
+            float: left;
+            list-style: none;
+            }
+          .maplegend .legend-scale ul li {
+            font-size: 80%;
+            list-style: none;
+            margin-left: 0;
+            line-height: 18px;
+            margin-bottom: 1px;
+            }
+          .maplegend ul.legend-labels li span {
+            display: block;
+            float: left;
+            height: 16px;
+            width: 30px;
+            margin-right: 5px;
+            margin-left: 0;
+            border: 1px solid #999;
+            }
+          .maplegend .legend-source {
+            font-size: 80%;
+            color: #777;
+            clear: both;
+            }
+          .maplegend a {
+            color: #777;
+            }
+        </style>
+        """
+
+        return speeding_legend_html
+
+    @staticmethod
     def _create_plp_layer(
         gdf: gpd.GeoDataFrame,
         point_color: str = "blue",
@@ -553,6 +640,10 @@ class Karta:
 
         # Instantiate base map and render all generated layers
         karta = Karta._base_map()
+
+        if point_color == speed_field:
+            speeding_legend_html = Karta._create_speeding_legend()
+            karta.get_root().html.add_child(folium.Element(speeding_legend_html))
 
         for layer, layer_name in zip(layers, layer_names):
             folium.FeatureGroup(name=layer_name).add_child(layer).add_to(karta)
